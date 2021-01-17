f19e090c65ded3f0feb82d22f664aec51411e5b1
zaighumg20 <77337297+zaighumg20@users.noreply.github.com>
2021-01-17 16:07:03 -0500

    Add files via upload

---
+++ Assigments/Assigment2/mad-libs.py
@@ -0,0 +1,11 @@
+print(" Tell us About You")
+
+CollegeName =input("Enter your College name: ")
+Adjective1 =input("Enter your adjective name: ")
+BodyPart =input("Enter your Body Part name: ")
+Object =input("Enter your Object name: ")
+Smell =input("Enter your Smell name: ")
+Action= input("Enter your Action name: ")
+Adjective2= input("Enter your Adjective name: ")
+
+print(f"It was the one night everybody dreads, the night before dark week starts- AKA finals week- at {CollegeName}. The library was full of {Adjective1} students all glued to their books and {BodyPart} deep in energy drink cans and empty coffee cups. One desperate student even had the guts to sneak in an {Object}. As expected, I couldn't find a decent place to sit so I had to sit next to the dude who smelled like {Smell} . I began to {Action} {Adjective2} .")
